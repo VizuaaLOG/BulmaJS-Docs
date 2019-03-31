2c90586bcad7c656453225c95636d2686de203af
Thomas Erbe <vizuaalog@gmail.com>
2019-03-31 14:36:59 +0100

    Move font awesome script to own file

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -1,14 +1,3 @@
-import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { faJs } from '@fortawesome/free-brands-svg-icons';
-import { faPlug } from '@fortawesome/free-solid-svg-icons';
-import { faPuzzlePiece } from '@fortawesome/free-solid-svg-icons';
-import { faDollarSign } from '@fortawesome/free-solid-svg-icons';
-import { faBan } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faJs);
-library.add(faPlug);
-library.add(faPuzzlePiece);
-library.add(faDollarSign);
-library.add(faBan);
+require('./fontawesome');
 
-dom.watch();
