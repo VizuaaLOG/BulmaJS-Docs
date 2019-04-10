85c15987e2149ff595ccd529dca0de234fec36de
Thomas Erbe <vizuaalog@gmail.com>
2019-04-10 21:38:15 +0100

    Docs nav fixes

--- source/_partials/docs_end_nav.blade.php
+++ source/_partials/docs_end_nav.blade.php
@@ -1,11 +1,11 @@
 <div class="docs-nav">
     <div class="docs-nav-item">
         @if($page->prev)
-            <a class="docs-nav-prev docs-nav-item" href="{{ $page->prev[1] }}">{{ $page->prev[0] }}</a>
+            <a class="docs-nav-prev" href="{{ $page->prev[1] }}">{{ $page->prev[0] }}</a>
         @endif
     </div>
     
     <div class="docs-nav-item">
-        <a class="docs-nav-next docs-nav-item" href="{{ $page->next[1] }}">{{ $page->next[0] }}</a>
+        <a class="docs-nav-next" href="{{ $page->next[1] }}">{{ $page->next[0] }}</a>
     </div>
 </div>
