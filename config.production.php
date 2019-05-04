e914863f3315424ebaf6df6db9a6405464dc06e9
Thomas Erbe <vizuaalog@gmail.com>
2019-05-04 19:48:30 +0100

    Update config.production.php

--- config.production.php
+++ config.production.php
@@ -2,4 +2,19 @@
 
 return [
     'production' => true,
+    'baseUrl' => '',
+    'collections' => [],
+    'versions' => [
+        'master',
+        '0.10',
+        '0.9',
+        '0.8',
+        '0.7',
+    ],
+    'github_issues_url' => 'https://github.com/VizuaaLOG/BulmaJS/issues',
+    'released_version' => '0.10.0',
+    'documentation_version' => '0.10',
+    'is' => function ($page, $section) {
+        return str_contains($page->getPath(), $section) ? 'is-active' : '';
+    },
 ];
